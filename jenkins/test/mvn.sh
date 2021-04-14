e914867d2e2a1ac50c0336a6543b5729530053c9
root <root@ip-172-31-3-205.ec2.internal>
2021-04-14 10:05:12 +0000

    Updated

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,6 @@ echo "**************************"
 echo "***Testing the code **********"
 echo "***************************"
 
+WORKSPACE=/home/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
