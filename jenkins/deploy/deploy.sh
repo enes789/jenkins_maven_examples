6f04474f15380194c7f02e2b0b274a086a3e1677
root <root@ip-172-31-3-205.ec2.internal>
2021-04-14 10:40:52 +0000

    Upp

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-scp -i /opt/prod /tmp/.auth produser@54.242.104.185:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh produser@54.242.104.185:/tmp/publish.sh
-ssh -i /opt/prod produser@54.242.104.185 "/tmp/publish.sh"
+scp -i /opt/prod /tmp/.auth produser@18.234.149.11:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh produser@18.234.149.11:/tmp/publish.sh
+ssh -i /opt/prod produser@18.234.149.11 "/tmp/publish.sh"
